b35f0617283cd716bc0882fd5ff12ae0cc07eae5
runba3011 <runba3011@gmail.com>
2021-09-05 21:58:26 +0900

    コメント投稿機能の使用制限の追加

--- app/controllers/stickman_war_comments_controller.rb
+++ app/controllers/stickman_war_comments_controller.rb
@@ -2,15 +2,15 @@ class StickmanWarCommentsController < ApplicationController
 
   def create
     @comment = StickmanWarComment.new(stickman_war_comment_params)
-    binding.pry
     if @comment.valid?
       @comment.save
       redirect_to stickman_war_stickman_war_detail_path(params[:stickman_war_id] , params[:stickman_war_detail_id])
     else
-      binding.pry
       @comments = StickmanWarComment.where(stage_type: params[:stickman_war_id] , stage_id: params[:stickman_war_detail_id])
       @create_path = stickman_war_stickman_war_detail_stickman_war_comments_path(params[:stickman_war_id] , params[:stickman_war_detail_id]) 
-      render "stickman_war_detail/#{params[:stickman_war_id]}/stage#{params[:stickman_war_detail_id]}"
+      @show_page_path = "stickman_war_detail/#{params[:stickman_war_id]}/stage#{params[:stickman_war_detail_id]}"
+      @comments = StickmanWarComment.where(stage_type: params[:stickman_war_id] , stage_id: params[:stickman_war_detial_id])
+      render "stickman_war_detail/show"
     end
   end
 
