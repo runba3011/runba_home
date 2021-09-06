19f66fa1bbd1ff8fefba421f3554caa3e8958057
runba3011 <runba3011@gmail.com>
2021-09-06 14:54:06 +0900

    Groups , Messagesコントローラーにアクションの記載

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,11 @@
 class MessagesController < ApplicationController
+  def create
+    
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id , group_id: params[:group_id])
+  end
 end
