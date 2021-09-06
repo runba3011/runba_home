eeb6ab3273afde857e20dafb7277874932f81ee2
runba3011 <runba3011@gmail.com>
2021-09-06 10:53:02 +0900

    ActiveHashを用いたMoviesモデルの作成

---
+++ app/models/movies.rb
@@ -0,0 +1,19 @@
+class Movies < ActiveHash::Base
+  # 後ろの１１桁の文字列のみurlの部分に入れる
+  self.data = [
+    {id: 1 , url: nil , reccomend: true , title: "1つめの動画" , url: "AQO0OR-LTQQ"} , 
+    {id: 2 , url: nil , reccomend: true , title: "2つめの動画" , url: "AQO0OR-LTQQ"} , 
+    {id: 3 , url: nil , reccomend: true , title: "3つめの動画" , url: "AQO0OR-LTQQ"} , 
+    {id: 4 , url: nil , reccomend: true , title: "4つめの動画" , url: "AQO0OR-LTQQ"} , 
+    {id: 5 , url: nil , reccomend: true , title: "5つめの動画" , url: "AQO0OR-LTQQ"} , 
+    {id: 6 , url: nil , reccomend: true , title: "6つめの動画" , url: "AQO0OR-LTQQ"} , 
+    {id: 7 , url: nil , reccomend: true , title: "7つめの動画" , url: "AQO0OR-LTQQ"} , 
+    {id: 8 , url: nil , reccomend: true , title: "8つめの動画" , url: "AQO0OR-LTQQ"} , 
+    {id: 9 , url: nil , reccomend: true , title: "9つめの動画" , url: "AQO0OR-LTQQ"} , 
+    {id: 10 , url: nil , reccomend: true , title: "10つめの動画" , url: "AQO0OR-LTQQ"} , 
+    {id: 11 , url: nil , reccomend: true , title: "11つめの動画" , url: "AQO0OR-LTQQ"} , 
+    {id: 12 , url: nil , reccomend: true , title: "12つめの動画" , url: "AQO0OR-LTQQ"} , 
+
+  ]
+
+end
