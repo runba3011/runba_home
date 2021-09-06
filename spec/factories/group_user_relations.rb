1acfb9d308d0804e1c0554311ff20b2181080b33
runba3011 <runba3011@gmail.com>
2021-09-06 18:36:17 +0900

    作成したモデルのFactoryBot処理を作成

--- spec/factories/group_user_relations.rb
+++ spec/factories/group_user_relations.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :group_user_relation do
     association :user
     association :group
-    authority_id {3}
+    authority_id {Faker::Number.between(from: 1 , to:5)}
   end
 end
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :message do
-    
+    text {Faker::Rorem.sentence}
+    association :user
+    association :group
   end
 end
