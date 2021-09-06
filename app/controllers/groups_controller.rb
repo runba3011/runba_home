2fdc510e4d2791f47c984c659fd1aa331c13aec7
runba3011 <runba3011@gmail.com>
2021-09-06 15:25:27 +0900

    ビューファイルの作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
+  before_action :generate_message , :generate_room 
+
+
   def index
     
   end
@@ -26,4 +29,15 @@ class GroupsController < ApplicationController
   def destroy
     
   end
+
+  private 
+
+  def generate_room
+    @room = Room.new
+  end
+
+  def generate_message
+    @message = Message.new
+  end
+
 end
