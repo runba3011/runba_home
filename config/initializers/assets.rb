ee3a2955212cc87eb4c31e5fdf7ec3887f3742ac
runba3011 <runba3011@gmail.com>
2021-09-07 22:03:37 +0900

    拡張子をscssに変更しても成功した

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( 
   top/index
+  shared/link
 )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
