3ac728a8c5cc25e63d75e44dc3d3408d76073bf5
runba3011 <runba3011@gmail.com>
2021-09-07 12:02:11 +0900

    グループに追加するユーザー選択の機能を実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :generate_message , :generate_room 
+  before_action :check_logined
+  before_action :generate_message , :generate_group 
 
 
   def index
@@ -11,7 +12,11 @@ class GroupsController < ApplicationController
   end
 
   def new
-    
+    @users = User.where.not(id: current_user.id)
+    @authorities = []
+    4.times do |i|
+      @authorities.unshift(Authority.data.detect{|o| o[:id] == i + 1})
+    end
   end
 
   def create
@@ -32,8 +37,14 @@ class GroupsController < ApplicationController
 
   private 
 
-  def generate_room
-    @room = Room.new
+  def check_logined
+    if !user_signed_in?
+      redirect_to users_path
+    end 
+  end
+
+  def generate_group
+    @group = Group.new
   end
 
   def generate_message
