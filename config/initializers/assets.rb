c22ad329d0c6921b72aa962f756ca774bba5c348
runba3011 <runba3011@gmail.com>
2021-09-08 11:37:52 +0900

    フッター部分の作成

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( 
   top/index
   shared/link
+  shared/footer
+  shared/simple_link
 )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
