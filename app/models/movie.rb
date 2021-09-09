93c9807b42e47f17caef49b49a650b1356ca46f2
runba3011 <runba3011@gmail.com>
2021-09-09 12:48:29 +0900

    動画一覧部分のCSSの作成

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveHash::Base
   # 後ろの１１桁の文字列のみlinkの部分に入れる
   self.data = [
     {id: 1 , reccomend: true , title: "【AIきりたん】恋色マスタースパーク" , link: "AQO0OR-LTQQ" , image: nil , explain: "説明を書こう！" , small_explain: "自分の動画を見ることが少ないルンバ氏が、何度も見たくなるレベルにおすすめ。実のところを申しますと解説系の動画って後からまたみてみると「なにこいつ偉そうに言ってるんだろう」って思って複雑な気持ちになるんですよね。"} , 
-    {id: 2 , reccomend: true , title: "2つめの動画" , link: "KBclKqeILkU" , image: nil , explain: "説明を書こう！" , small_explain: nil} , 
+    {id: 2 , reccomend: true , title: "ためしにすごいほどにながいたいとるをつけてみよう！やじゅうせんぱいばんざい！やじゅうせんぱいばんざい！" , link: "KBclKqeILkU" , image: nil , explain: "説明を書こう！" , small_explain: nil} , 
     {id: 3 , reccomend: true , title: "3つめの動画" , link: "AQO0OR-" , image: nil , explain: "説明を書こう！" , small_explain: nil} , 
     {id: 4 , reccomend: true , title: "4つめの動画" , link: "AQO0OR-" , image: nil , explain: "説明を書こう！" , small_explain: nil} , 
     {id: 5 , reccomend: true , title: "5つめの動画" , link: "AQO0OR-" , image: nil , explain: "説明を書こう！" , small_explain: nil} , 
