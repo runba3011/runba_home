7ebf316483c75c41400c7fe30d27427afa513ab9
runba3011 <runba3011@gmail.com>
2021-09-09 15:36:41 +0900

    コメント入力欄に長いものを入力した際、入力欄が縦に伸びる仕組みの作成

---
+++ app/javascript/adjust_height.js
@@ -0,0 +1,25 @@
+function adjust_height(){
+  if(document.URL.match(/movies/)){
+    //textareaの要素を取得
+    let textarea = document.getElementById("use_adjust");
+    //textareaのデフォルトの要素の高さを取得
+    let clientHeight = textarea.clientHeight;
+  
+    console.log(textarea);
+  
+    //textareaのinputイベント
+    textarea.addEventListener("input", ()=>{
+      console.log("入力されました");
+      //textareaの要素の高さを設定（rows属性で行を指定するなら「px」ではなく「auto」で良いかも！）
+      textarea.style.height = clientHeight + 'px';
+      //textareaの入力内容の高さを取得
+      let scrollHeight = textarea.scrollHeight;
+      //textareaの高さに入力内容の高さを設定
+      textarea.style.height = scrollHeight + 'px';
+    });
+  }
+}
+
+
+
+window.addEventListener("load" , adjust_height)
