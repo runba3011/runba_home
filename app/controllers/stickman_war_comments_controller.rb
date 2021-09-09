568b3d4f543d4a07c884b6ed963305e839b7d5b7
runba3011 <runba3011@gmail.com>
2021-09-09 11:14:01 +0900

    コメント削除機能の作成

--- app/controllers/stickman_war_comments_controller.rb
+++ app/controllers/stickman_war_comments_controller.rb
@@ -9,9 +9,23 @@ class StickmanWarCommentsController < ApplicationController
       @comments = StickmanWarComment.where(stage_type: params[:stickman_war_id] , stage_id: params[:stickman_war_detail_id])
       @create_path = stickman_war_stickman_war_detail_stickman_war_comments_path(params[:stickman_war_id] , params[:stickman_war_detail_id]) 
       @show_page_path = "stickman_war_detail/#{params[:stickman_war_id]}/stage#{params[:stickman_war_detail_id]}"
-      # binding.pry
+
+      @stickman_war_stage_type = params[:stickman_war_id]
+      @stickman_war_stage_number = params[:stickman_war_detail_id]
       render "stickman_war_detail/show"
     end
+  end
+
+  def destroy
+    binding.pry
+    @comment = StickmanWarComment.find(params[:id])
+    if @comment.user == current_user
+      @comment.destroy
+    end
+
+    @stickman_war_stage_type = params[:stickman_war_id]
+    @stickman_war_stage_number = params[:stickman_war_detail_id]
+    redirect_to stickman_war_stickman_war_detail_path(params[:stickman_war_id] , params[:stickman_war_detail_id])
   end
 
   private
