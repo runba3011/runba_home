0e1b62b03fa1f6085521f845ba9e6393f842c88d
runba3011 <runba3011@gmail.com>
2021-09-10 17:29:38 +0900

    メンバーを複数人追加する処理の作成

--- app/javascript/add_member.js
+++ app/javascript/add_member.js
@@ -1,16 +1,27 @@
+let created_amount = 0;
+
 function add_member(){
   if(document.URL.match(/groups/) ){
-    console.log("野獣先輩");
+
     const add_button = document.getElementById("add_member");
-    const add_object = document.getElementById("root_member_box");
+    const root_add_object = document.getElementById("root_member_box");
     const add_point = document.getElementById("add_member");
 
     add_button.addEventListener('click' , ()=>{
-      add_point.insertAdjacentHTML("beforebegin" , add_object.outerHTML);
+      created_amount += 1;
+      console.log(created_amount);
+      let add_object = root_add_object.outerHTML;
+      next_id = "delete_button"+created_amount
+      add_object = add_object.replace("delete_button0" , next_id)
+      // debugger;
+      individual_delete_button = add_object.lastElementChild;
+      console.log(individual_delete_button);
+      
+      add_point.insertAdjacentHTML("beforebegin" , add_object);
     });
   };
 };
 
-
-
 window.addEventListener("load" , add_member);
+
+
