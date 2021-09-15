47682b07f362a1ea0e46b9fdc44c9b2a700584bd
runba3011 <runba3011@gmail.com>
2021-09-15 17:28:52 +0900

    アソシエーション、バリデーションの設定

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,2 +1,9 @@
 class Follower < ApplicationRecord
+  with_options presence: true do
+    validates :follower
+    validates :user
+  end
+
+  belongs_to :user
+  belongs_to :following , class_name :"User"
 end
