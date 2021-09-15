fb483c337fe5f4ed6fb691c2bd44f917236c0c61
runba3011 <runba3011@gmail.com>
2021-09-15 11:20:58 +0900

    userコントローラー内に大まかな処理の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+  before_action :get_user , only: [:show , :edit]
+  before_action :check_same_user , only: :edit
+  
   def index
     flash[:notice] = nil
+  end
+
+  def show
+
+  end
+
+  def edit 
+
+  end
+
+  private 
+
+  def get_user
+    @user = User.find(params[:id])
+  end
+
+  def check_same_user
+    if @user != current_user
+      redirect_to root_path
+    end
   end
 end
