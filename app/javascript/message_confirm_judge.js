a4d41b0a10fe136e6c57cf35a2bc7813dc32690e
runba3011 <runba3011@gmail.com>
2021-09-16 17:22:35 +0900

    確認ウィンドウ削除処理の修正

--- app/javascript/message_confirm_judge.js
+++ app/javascript/message_confirm_judge.js
@@ -3,7 +3,7 @@ function judge(){
     const deleteButton = document.getElementById("confirm_delete");
     deleteButton.addEventListener('click' , ()=>{
       const confirmWindowParent = document.getElementById("confirm_parent");
-      confirmWindowParent.setAttribute('class' , 'hidden')
+      confirmWindowParent.classList.add('hidden')
       const messageImage = document.getElementById("message_image");
       messageImage.value = null;
       // 要確認
