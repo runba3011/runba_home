fa3577432f2f194b5c1458767357d0e96da9b38b
runba3011 <runba3011@gmail.com>
2021-09-20 18:20:51 +0900

    リダイレクトが行われた場合、元の位置の留まる位置（未開発）

---
+++ app/javascript/authority_scroll.js
@@ -0,0 +1,33 @@
+let positionX = 0;
+let positionY = 0;
+
+function getElements(){
+  const positionPoint = document.getElementById("position_point");
+  const members = document.getElementById("messages_parent");
+  const newMember = document.getElementById("new_user_box");
+  const firstNewMemberPointY = newMember.getBoundingClientRect().top;
+  
+  function getPosition(){
+    newMemberY = newMember.getBoundingClientRect().top;
+    let positionPointY = positionPoint.getBoundingClientRect().top;
+    authorityPoint = firstNewMemberPointY - newMemberY;
+    console.log(authorityPoint);
+
+    function autoScroll(){
+      window.alert("テスト");
+      console.log("遷移");
+      members.scrollTo(0,authorityPoint);
+    }
+
+    window.addEventListener("beforeunload", autoScroll);
+  };
+  members.addEventListener('scroll', getPosition);
+};
+
+
+
+function setPosition(){
+}
+
+
+window.addEventListener('load' , getElements);
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ require("../change_icon_image")
 require("../change_background_image")
 require("../message_image")
 require("../message_confirm_judge")
+require("../authority_scroll")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
