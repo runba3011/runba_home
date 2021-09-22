60b4a737a8d81f814b379bfd035957e99aff2011
runba3011 <runba3011@gmail.com>
2021-09-22 10:56:50 +0900

    fontawesomeの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,8 @@ require("../message_image")
 require("../message_confirm_judge")
 require("../authority_scroll")
 require("../messages_scroll_bottom")
+import '@fortawesome/fontawesome-free/js/all';
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
