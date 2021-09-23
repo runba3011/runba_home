4c09306ac2d81cf0a670b0c1c83a8dd895784a9d
runba3011 <runba3011@gmail.com>
2021-09-23 15:42:01 +0900

    設定した背景画像が反映されない不具合の調整

--- app/javascript/change_background_image.js
+++ app/javascript/change_background_image.js
@@ -1,29 +1,32 @@
 function change_background(){
-  if(document.URL.match(/user/) && document.URL.match(/edit/)){
+  if(document.URL.match(/user/)){
     const backgroundForm = document.getElementById("background_image_field");
-    backgroundForm.addEventListener('change' , (e)=>{
-      // 挿入先の検索、取得できていることは確認済み
-      const backgroundImageParent = document.getElementById("background_image_parent");
-      const image = e.target.files[0];
-      const imageURL = window.URL.createObjectURL(image);
-
-      const imageElement = document.createElement('img');
-      imageElement.setAttribute('src' , imageURL);
-      imageElement.setAttribute('class', "top_image");
-      imageElement.setAttribute('id' , "top_image")
-
-      removeObject = document.getElementById("top_image");
-      removeObject.remove();
-      backgroundImageParent.appendChild(imageElement);
-
-      // 選択した画像を表示する処理の完成
-
-      // ここからは編集部分だとわかりやすくする部分の作成
-      const backgroundEasy = document.getElementById("background_easy");
-      if(backgroundEasy != null){
-        backgroundEasy.remove();
-      };
-    });
+    if(backgroundForm != null){
+      backgroundForm.addEventListener('change' , (e)=>{
+        console.log("変更");
+        // 挿入先の検索、取得できていることは確認済み
+        const backgroundImageParent = document.getElementById("background_image_parent");
+        const image = e.target.files[0];
+        const imageURL = window.URL.createObjectURL(image);
+  
+        const imageElement = document.createElement('img');
+        imageElement.setAttribute('src' , imageURL);
+        imageElement.setAttribute('class', "top_image");
+        imageElement.setAttribute('id' , "top_image")
+  
+        removeObject = document.getElementById("top_image");
+        removeObject.remove();
+        backgroundImageParent.appendChild(imageElement);
+  
+        // 選択した画像を表示する処理の完成
+  
+        // ここからは編集部分だとわかりやすくする部分の作成
+        const backgroundEasy = document.getElementById("background_easy");
+        if(backgroundEasy != null){
+          backgroundEasy.remove();
+        };
+      });
+    }
   };
 };
 
