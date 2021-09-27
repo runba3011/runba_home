b77df53441f9dcd18863f5e86a2bf57430586607
runba3011 <runba3011@gmail.com>
2021-09-27 11:28:54 +0900

    正しい動画が取得できない不具合の強引な修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,10 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.find(params[:id])
+    # binding.pry
+    if @movie.id != params[:id].to_i
+      @movie = Movie.find(Movie.all.length-1 - params[:id].to_i)
+    end
     @movie_comment = MovieComment.new
     # binding.pry
   end
