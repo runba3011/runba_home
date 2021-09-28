0dae578e4d30a6c0b00d7eb9b7edf0ab949d0b2e
runba3011 <runba3011@gmail.com>
2021-09-28 18:20:16 +0900

    アクティブハッシュの作成

---
+++ app/models/skill_all_language.rb
@@ -0,0 +1,10 @@
+class SkillAllLanguage < ActiveHash::Base
+  self.data = [
+    {id: 1 , title: "HTML&CSSとは？"},
+    {id: 2 , title: "JavaScriptとは？"},
+    {id: 3 , title: "Ruby on Railsとは？"},
+    {id: 4 , title: "Unityとは？"},
+    
+  ]
+
+end
---
+++ app/models/skill_jquery.rb
@@ -0,0 +1,6 @@
+class SkillJquery < ActiveHash::Base
+  self.data = [
+    {id: 1 , title: "正直何をやるのかわかってない" , explain: "詳しい説明を書こう！"}
+  ]
+
+end
