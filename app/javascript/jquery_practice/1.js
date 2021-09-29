1d311f3d3f053d518bca658874197900042cfee6
runba3011 <runba3011@gmail.com>
2021-09-29 14:51:22 +0900

    rails-ujsによって動かなくなった

--- app/javascript/jquery_practice/1.js
+++ app/javascript/jquery_practice/1.js
@@ -1,9 +1,14 @@
+import { $ } from "@rails/ujs";
+
 function test1(){
   if(document.URL.match(/skill_room/) && document.URL.match(/jquery_practice/)){
     console.log("スクリプトを使用します");
     $(document).ready(function(){
       // jqueryの導入ができていることのテスト
-      $("#jquery_test").text("jQueryテスト（稼働中）")
+      $("#jquery_test").text("jQuery稼働テスト（成功！）");
+
+      $("#jquery_id_get").text("ID取得テスト（成功！）");
+      $("section").text("要素名取得テスト（成功！）");
     })
   }
 }
