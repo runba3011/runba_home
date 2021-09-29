365c1a721edcec200e4a7660a2b1528a91543cfe
runba3011 <runba3011@gmail.com>
2021-09-29 11:30:56 +0900

    基本的な表示の作成

--- app/controllers/skill_room_controller.rb
+++ app/controllers/skill_room_controller.rb
@@ -5,13 +5,13 @@ class SkillRoomController < ApplicationController
 
   def show
     if params[:type_id] == "html_css_explain"
-      render "skill_room/explain/html_css"
+      @html_css_explain = true;
     elsif params[:type_id] == "java_script_explain"
-      render "skill_room/explain/javascript"
+      @javascript_explain = true;
     elsif params[:type_id] == "ruby_on_rails_explain"
-      render "skill_room/explain/ruby_on_rails"
+      @ruby_on_rails_explain = true;
     elsif params[:type_id] == "unity_explain"
-      render "skill_room/explain/unity"
+      @unity_explain = true;
     # ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ここまでは言語の説明のリンク
     elsif params[:type_id] == "jquery_practice"
     elsif params[:type_id] == "javascript_explain"
