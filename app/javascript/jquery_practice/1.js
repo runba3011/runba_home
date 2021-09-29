57bac5cd4eb02857830388fb8162fd220a718a9c
runba3011 <runba3011@gmail.com>
2021-09-29 16:28:59 +0900

    rials-ujsに抵抗する術を発見

--- app/javascript/jquery_practice/1.js
+++ app/javascript/jquery_practice/1.js
@@ -1,17 +1,19 @@
+import { $ } from "@rails/ujs";
+
 function test1(){
   if(document.URL.match(/skill_room/) && document.URL.match(/jquery_practice/)){
     console.log(`スクリプトを使用します`);
-    $(document).ready(function(){
+    jQuery(function($){
       // jqueryの導入ができていることのテスト
       function setText(text){
-        console.log(`関数の呼び出しに成功しました`);
-        $(`#jquery_test`).text(`jQuery稼働テスト（${text}）`);
+          console.log(`関数の呼び出しに成功しました`);
+          $(`#jquery_test`).text(`jQuery稼働テスト（成功！）`);
 
-        $(`#jquery_id_get`).text(`ID取得テスト（${text}）`);
-        $(`section`).text(`要素名取得テスト（${text}）`);
-        // debugger;
-        $(`.jquery_class_get`).text(`クラス取得テスト(${text})`);
-        $(`a[href$='_practice/1']`).text(`URL取得テスト（${text}）`);
+          $(`#jquery_id_get`).text(`ID取得テスト（${text}）`);
+          $(`section`).text(`要素名取得テスト（${text}）`);
+          // debugger;
+          $(`.jquery_class_get`).text(`クラス取得テスト(${text})`);
+          $(`a[href$='_practice/1']`).text(`URL取得テスト（${text}）`);
       }
       $(setText("成功！"));
       $(setText("関数からの呼び出しに成功！"));
@@ -19,7 +21,7 @@ function test1(){
       function addHTML(html){
         $(`${html}`).appendTo("#family");
       }
-      $(addHTML("<li>新しい要素だよ！</li>"));
+      addHTML("<li>新しい要素だよ！</li>");
     })
   }
 }
