912d3b707977336ba522a72cc004ce99c12841a3
runba3011 <runba3011@gmail.com>
2021-09-29 10:28:21 +0900

    言語解説の部分の見た目の調整

--- app/models/skill_all_language.rb
+++ app/models/skill_all_language.rb
@@ -2,8 +2,8 @@ class SkillAllLanguage < ActiveHash::Base
   self.data = [
     {id: 1 , title: "HTML&CSSとは？" , link: "/skill_room/html_explain" , image: "skill_room/top/html_css"},
     {id: 2 , title: "JavaScriptとは？" , link: "/skill_room/java_script_explain" , image: "skill_room/top/javascript"},
-    {id: 3 , title: "Ruby on Railsとは？" , link: "/skill_room/ruby_on_rails_explain" , image: nil},
-    {id: 4 , title: "Unityとは？" , link: "/skill_room/unity_explain" , image: nil},
+    {id: 3 , title: "Ruby on Railsとは？" , link: "/skill_room/ruby_on_rails_explain" , image: "skill_room/top/ruby"},
+    {id: 4 , title: "Unityとは？" , link: "/skill_room/unity_explain" , image: "skill_room/top/unity"},
     # {id: 2 , title: "JavaScriptとは？"},
     # {id: 3 , title: "Ruby on Railsとは？"},
     # {id: 4 , title: "Unityとは？"},
