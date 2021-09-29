c88cc517c2ad7c6a6dafd3968b9b12dcdf7b0d94
runba3011 <runba3011@gmail.com>
2021-09-29 15:29:51 +0900

    関数の使用、IDによる取得、classによる取得、要素名による取得、URLによる取得のテスト

--- app/javascript/jquery_practice/1.js
+++ app/javascript/jquery_practice/1.js
@@ -1,14 +1,24 @@
+// import { $ } from "@rails/ujs";
+
 function test1(){
   if(document.URL.match(/skill_room/) && document.URL.match(/jquery_practice/)){
-    console.log("スクリプトを使用します");
+    console.log(`スクリプトを使用します`);
     $(document).ready(function(){
       // jqueryの導入ができていることのテスト
-      $("#jquery_test").text("jQuery稼働テスト（成功！）");
+      function setText(text){
+        console.log(`関数の呼び出しに成功しました`);
+        $(`#jquery_test`).text(`jQuery稼働テスト（${text}）`);
 
-      $("#jquery_id_get").text("ID取得テスト（成功！）");
-      $("section").text("要素名取得テスト（成功！）");
+        $(`#jquery_id_get`).text(`ID取得テスト（${text}）`);
+        $(`section`).text(`要素名取得テスト（${text}）`);
+        // debugger;
+        $(`.jquery_class_get`).text(`クラス取得テスト(${text})`);
+        $(`a[href$='_practice/1']`).text(`URL取得テスト（${text}）`);
+      }
+      $(setText("成功！"));
+      $(setText("関数からの呼び出しに成功！"));
     })
   }
 }
 
-window.addEventListener("load" , test1)
+window.addEventListener(`load` , test1)
