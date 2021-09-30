7018cfc2764c2831247a588592c783ac89e2d1f4
runba3011 <runba3011@gmail.com>
2021-09-30 16:38:07 +0900

    contain , has , empty , parentについての学習

--- app/javascript/jquery_practice/3.js
+++ app/javascript/jquery_practice/3.js
@@ -9,5 +9,14 @@ jQuery(function($){
       $("#contain_has_test_parent ~ .contain_has_parent_explain2").text("<span>のある、結月ゆかり、弦巻まき、京町せいか、琴葉あおいの背景が青くなっています");
     }
     containHasTest();
+
+    function emptyParentTest(){
+      $(".empty_parent_test").css("width" , "33.33333333%").css("height" , "30px").css("display" , "inline-block").css("text-align" , "center");
+      $(".empty_parent_test:empty").css("background-color" , "gray");
+      $(".empty_parent_test:parent").css("background-color" , "yellow");
+      $(".empty_parent_test_explain:eq(0)").text("「要素」などの文字列が入っているところの背景は黄色に").css("display" , "block");
+      $(".empty_parent_test_explain:eq(1)").text("入っていないところはグレーになっている").css("display" , "block");
+    }
+    emptyParentTest();
   }
 })
