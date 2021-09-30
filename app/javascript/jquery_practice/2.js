d193daf2709f27b1db75ff924f92d4ef053fb2c0
runba3011 <runba3011@gmail.com>
2021-09-30 12:48:33 +0900

    オブジェクトの最初や最後の取得方法のテスト

--- app/javascript/jquery_practice/2.js
+++ app/javascript/jquery_practice/2.js
@@ -2,4 +2,24 @@ jQuery(function($){
   if(document.URL.match(/skill_room/) && document.URL.match(/2/) && document.URL.match(/jquery_practice/)){
     console.log("jQuery練習用スクリプト2が稼働しています")
   }
+
+  function findFirst(){
+    $(".jquery_practice_class1:first").css("font-weight" , "bold");
+    $(".jquery_practice_class1:first-child").css("color" , "blue");
+    $(".jquery_practice_detail > span").text("要素１と要素４が青文字になり、要素１のみが太文字になる").css("display" , "block");
+  }
+  findFirst();
+
+  function findLast(){
+    $(".jquery_practice_class1:last-child").css("border" , "1px dotted pink").css("margin" , "3px auto");
+    $(".jquery_practice_class1:last").css("border-bottom" , "3px solid red");
+    $("div .message2").text("要素３と要素６にピンクの境界線がつき、要素６には赤い下線がつく").css("display" , "block");
+  }
+  findLast();
+
+  function useNthChild(){
+    const objects = $(".jquery_practice_class2");
+    objects.css("width" , "25%").css("text-align" , "center").css("display" , "inline-block").css("margin" , "5px auto");
+  }
+  useNthChild();
 })
