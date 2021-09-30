2e6eec14f01f130d09ea782f449f8f89eeee4158
runba3011 <runba3011@gmail.com>
2021-09-30 15:17:38 +0900

    子要素フィルターの使用方法

--- app/javascript/jquery_practice/2.js
+++ app/javascript/jquery_practice/2.js
@@ -6,7 +6,7 @@ jQuery(function($){
   function findFirst(){
     $(".jquery_practice_class1:first").css("font-weight" , "bold");
     $(".jquery_practice_class1:first-child").css("color" , "blue");
-    $(".jquery_practice_detail > span").text("要素１と要素４が青文字になり、要素１のみが太文字になる").css("display" , "block");
+    $("#jquery_practice_id_3 > span").text("要素１と要素４が青文字になり、要素１のみが太文字になる").css("display" , "block");
   }
   findFirst();
 
@@ -26,7 +26,38 @@ jQuery(function($){
 
   function useNthChild(){
     const objects = $(".jquery_practice_class2");
-    objects.css("width" , "25%").css("text-align" , "center").css("display" , "inline-block").css("margin" , "5px auto");
+    $("#use_background_1").css("background-color" , "gray");
+    objects.css("width" , "25%").css("text-align" , "center").css("display" , "inline-block").css("padding" , "5px auto").css("margin" , "2px auto");
+    $(".jquery_practice_class2:nth-child(even)").css("background-color" , "#fcffdb");
+    $(".jquery_practice_class2:nth-child(odd)").css("background-color" , "#4560ff").css("color" , "white");
+    $(".jquery_practice_class2:nth-child(3n)").css("border" , "2px solid black");
+    $(".jquery_practice_class2:nth-child(3n+1)").css("border" , "2px solid green");
+    $(".jquery_practice_class2:nth-child(1)").css("border" , "2px solid red");
+    $(".nth_child_explain:eq(1)").text("要素１に赤い境界線がついている");
+    $(".nth_child_explain:eq(2)").text("偶数番目の背景色が薄い黄色になっている");
+    $(".nth_child_explain:eq(3)").text("奇数番目の背景が青くなっている");
+    $(".nth_child_explain:eq(4)").text("3の倍数番目に黒い境界線がついている");
+    $(".nth_child_explain:eq(5)").text("3で割った時1で余る番目に緑の境界線がついている");
+    $(".nth_child_explain").css("display" , "block");
   }
   useNthChild();
+
+  function useOdd(){
+    $(".jquery_practice_class4:odd").css("font-weight" , "bold").css("color" , "purple").css("width" , "25%").css("text-align" , "center").css("margin", "3px auto");
+    $(".jquery_practice_class4:even").css("font-weight" , "bold").css("color" , "red").css("width" , "25%").css("text-align" , "center").css("margin", "3px auto");
+    $("#jquery_practice_id_2 ~ span").text("偶数番目の文字が赤色になり、奇数番目の文字が紫色になる");
+  }
+  useOdd();
+
+  function useEqLtGt(){
+    $(".jquery_practice_class5:eq(0)").css("font-weight" , "bold");
+    $(".jquery_practice_class5:gt(3)").css("background-color" , "#e8abff");
+    $(".jquery_practice_class5:lt(5)").css("border" , "2px solid blue");
+    $(".jquery_practice_class5").css("width" , "25%").css("margin" , "3px auto");
+    $(".eq_explain_span:nth-child(4)").text("１番目の文字が太くなっている");
+    $(".eq_explain_span:nth-child(2)").text("４番より後の文字に紫の背景がついている");
+    $(".eq_explain_span:nth-child(3)").text("６番より前の文字に青い境界線がついている");
+    $(".eq_explain_span").css("display" , "block");
+  }
+  useEqLtGt();
 })
