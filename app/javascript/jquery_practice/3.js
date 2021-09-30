9d63cabf45b4a034e0cb96817ff96fa892378a7f
runba3011 <runba3011@gmail.com>
2021-09-30 15:34:39 +0900

    メインページに戻るボタンの追加、練習用ファイルの作成

---
+++ app/javascript/jquery_practice/3.js
@@ -0,0 +1,5 @@
+jQuery(function($){
+  if(document.URL.match(/skill_room/) && document.URL.match(/3/) && document.URL.match(/jquery_practice/)){
+    console.log("jQuery練習用スクリプト3が稼働しています")
+  }
+})
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,6 +23,7 @@ import '@fortawesome/fontawesome-free/js/all';
 
 require("../jquery_practice/1")
 require("../jquery_practice/2")
+require("../jquery_practice/3")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
