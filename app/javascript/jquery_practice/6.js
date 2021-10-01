f2e849a38640aa0caccab1d0b8de2f77510f08ab
runba3011 <runba3011@gmail.com>
2021-10-01 18:30:36 +0900

    読み込みのテスト

--- app/javascript/jquery_practice/6.js
+++ app/javascript/jquery_practice/6.js
@@ -2,7 +2,7 @@ jQuery(function($){
   if(document.URL.match(/skill_room/) && document.URL.match(/6/) && document.URL.match(/jquery_practice/)){
     if(document.getElementById("jquery_practice_6") != null){
       console.log("jQuery練習用スクリプト6が稼働しています");
-
+      $(".magic_test_1").addClass('magictime magic');
     }
   }
 })
