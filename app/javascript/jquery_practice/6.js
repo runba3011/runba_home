d5856378df0825b3173ae49d872953ac5d209721
runba3011 <runba3011@gmail.com>
2021-10-01 18:02:50 +0900

    テスト用ファイルの作成

---
+++ app/javascript/jquery_practice/6.js
@@ -0,0 +1,7 @@
+jQuery(function($){
+  if(document.URL.match(/skill_room/) && document.URL.match(/6/) && document.URL.match(/jquery_practice/)){
+    if(document.getElementById("jquery_practice_6") != null){
+
+    }
+  }
+})
