8aa42048380e011ff0c56b3b361f98cde85594a0
runba3011 <runba3011@gmail.com>
2021-10-04 19:43:54 +0900

    jquery をyarnを用いて管理するように変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//= require jquery
-//= require jquery_ujs
+require("jquery")
 require("@rails/ujs").start()
 // require("turbolinks").start()
+// require("../src/jquery.inview.min")
 
 require("@rails/activestorage").start()
 require("channels")
