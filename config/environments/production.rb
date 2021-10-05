8e06b673c509b015534b84c625456e0a1271adff
runba3011 <runba3011@gmail.com>
2021-10-05 16:34:00 +0900

    本番環境でリクエスト送信時にメールが送られるように変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      port: 587,
+      address: 'smtp.gmail.com',
+      domain: 'smtp.gmail.com',
+      user_name: "runba3011@gmail.com",
+      password: ENV["RUNBA_HOME_EMAIL_PASSWORD"],
+      enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
