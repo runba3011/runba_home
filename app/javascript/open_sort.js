0562db82ddab9dd85820ef6221140e9095ea8484
runba3011 <runba3011@gmail.com>
2021-10-12 14:33:25 +0900

    閉じるボタンを押した時、閉じるテキストの一番上へスクロールするように変更

--- app/javascript/open_sort.js
+++ app/javascript/open_sort.js
@@ -5,7 +5,6 @@ jQuery(function($){
     const $openSortButtonBorder = $(".open_sort_button_border");
 
     if($openSortButton.length >= 1){
-      console.log("並び替えボタンを開くやつ");
 
       $openSortButton.on("click" , function(){
         $openSortButtonBorder.css("opacity" , "1");
@@ -18,6 +17,54 @@ jQuery(function($){
         else{
           $openSortButton.text("並び替えを使用する");
         }
+      })
+
+      // 同じページで使用するスクリプトなので、
+      // あまりの長文を入力された時にスクロールバーや全文表示ボタンを
+      // そのオブジェクトにのみ表示するスクリプトをここに作成しておく
+
+      const $text = $(".request_content");
+      $text.each(function(index , element){
+        const maxHeight = 500;
+        const $element = $(element);
+        if($element.height() >= maxHeight){
+          console.log($element.height());
+          $element.css({
+            "overflow-y": "scroll" , 
+            "max-height": `${maxHeight}px`
+          });
+          $element.next().css("display" , "block");
+        }
+        else{
+          $element.css({
+            "overflow-y": "inherit" , 
+            "max-height": "inherit"
+          });
+        }
+
+        $element.next().on("click" , function(){
+          if($(this).text() != "閉じる"){
+            // 滑らかな動きにするためにmax-heightを一瞬だけ解放、その時の高さを取得して改めてmax-heightを設定する
+            $element.css({
+              "max-height":"inherit"
+            });
+  
+            const height = $element.height();
+            $element.css("max-height" , `${maxHeight}px`);
+  
+            $element.animate({"max-height": height} , 300 , function(){
+              $element.next().text("閉じる");
+            });
+          }
+          else{
+            const scrollHeight = $element.offset().top  - 150;
+            console.log(scrollHeight)
+            $('body , html').animate({scrollTop:scrollHeight} , 300)
+            $element.animate({"max-height":`${maxHeight}px`} , 300 , function(){
+              $element.next().text("全文を見る")
+            })
+          }
+        })
       })
     }
   }
