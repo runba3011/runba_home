2d17973d9d0fa6562bc0d6392290938d2a9e27dc
runba3011 <runba3011@gmail.com>
2021-10-12 11:21:32 +0900

    並び替えボタンを閉じる時、閉じるためっぽいラインが表示されるように変更

--- app/javascript/open_sort.js
+++ app/javascript/open_sort.js
@@ -2,12 +2,16 @@ jQuery(function($){
   if(document.URL.match(/users/) && document.URL.match(/all_user_requests/)){
     const $openSortButton = $(".open_sort_button");
     const $sortButtonParent = $(".sort_buttons")
+    const $openSortButtonBorder = $(".open_sort_button_border");
 
     if($openSortButton.length >= 1){
       console.log("並び替えボタンを開くやつ");
 
       $openSortButton.on("click" , function(){
-        $sortButtonParent.slideToggle();
+        $openSortButtonBorder.css("opacity" , "1");
+        $sortButtonParent.slideToggle(function(){
+          $openSortButtonBorder.animate({opacity: 0} , 300)
+        });
         if($openSortButton.text() != "閉じる"){
           $openSortButton.text("閉じる")
         }
