e581f438587096fb736580dde82bbc35f634ba5f
runba3011 <runba3011@gmail.com>
2021-10-12 10:57:13 +0900

    テキストも変更されるように処理を作成

--- app/javascript/channels/all_user_request_channel.js
+++ app/javascript/channels/all_user_request_channel.js
@@ -14,17 +14,25 @@ consumer.subscriptions.create("AllUserRequestChannel", {
     jQuery(function($){
 
       const $request_object = $(`.request_class_${data.request.id}`);
+      const $status_text = $(`.now_status_${data.request.id}`);
       console.log($request_object.length)
       
       $request_object.removeClass("finished_color received_color untreated_color");
+      
       if(data.request.status == 2){
         $request_object.addClass("finished_color");
+        $status_text.text("");
+        $status_text.prepend('現在の状況：<i class="fas fa-award status_awesome"></i></i>完了！');
       }
       else if(data.request.status == 1){
         $request_object.addClass("received_color");
+        $status_text.text('');
+        $status_text.prepend('現在の状況：<i class="fas fa-check status_awesome"></i>採用！');
       }
       else if(data.request.status == 0){
         $request_object.addClass("untreated_color");
+        $status_text.text("");
+        $status_text.prepend('現在の状況：<i class="fas fa-check status_awesome"></i>採用！');
       }
     })
   }
