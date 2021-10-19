b4c1a390adf71c5f812eec0c6cc4f725a568e548
runba3011 <runba3011@gmail.com>
2021-10-19 12:33:47 +0900

    コントローラー内でそれぞれのアイコンを定義

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -1,6 +1,8 @@
 class MaterialsController < ApplicationController
   def index
-    
+    @icon_public_domain_q = "materials/icon/public_domain_q.png"
+    @icon_mascle_plus = "materials/icon/mascle_plus.png"
+    @icon_topecon_heroes = "materials/icon/topecon_heroes.png"
   end
 
   def show
