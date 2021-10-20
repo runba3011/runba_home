d565079ac51f2b5906704f86b5931b4666a2f982
runba3011 <runba3011@gmail.com>
2021-10-20 11:23:50 +0900

    コントローラーで画像を定義

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -3,6 +3,9 @@ class MaterialsController < ApplicationController
     @icon_public_domain_q = "materials/icon/public_domain_q.png"
     @icon_mascle_plus = "materials/icon/mascle_plus.png"
     @icon_topecon_heroes = "materials/icon/topecon_heroes.png"
+    @icon_nanasan_yuki = "materials/icon/nanasan_yuki.png"
+    @icon_texture_ninja = "materials/icon/texture_ninja.png"
+    @icon_beiz_images = "materials/icon/beiz_images.png"
   end
 
   def show
